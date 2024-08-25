42685e3767b09fbc363be69e95b92fc78d422de8
Dmitry <hulk295@yandex.ru>
2024-08-26 01:05:42 +0300

    Задание 9-7 Домашнее задание по теме "Декораторы"; оптимизирована проверка на простоту (искать делители надо не до половины числа, а только до его квадратного корня)

--- module_9_7.py
+++ module_9_7.py
@@ -19,7 +19,7 @@ def is_prime(func):
         # str = f'Число {res_tmp}: простое'
         str = f'простое\n{res_tmp}'
         for i in range(2, res_tmp + 1):
-            if i < (res_tmp / 2) + 1:
+            if i < int(res_tmp ** 0.5) + 1:
                 if res_tmp % i == 0:
                     str = f'составное\n{res_tmp}'
                     break
